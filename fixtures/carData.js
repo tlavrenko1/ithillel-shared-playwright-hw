a091428a9c119713458b44647d1015fdcfa5f378
tlavrenko1 <lavrenko.tatianna@gmail.com>
2024-06-27 22:40:02 +0200

    done

---
+++ fixtures/carData.js
@@ -0,0 +1,14 @@
+export const carData = {
+    featured: [
+        {
+            brand: 1,
+            model: [1,2,3],
+            mileage: "100"
+        },
+        {
+            brand: 2,
+            model: [6,7,8,9],
+            mileage: "101"
+        }
+    ]
+};
